c04a3d337094df1e4ca18d5b5115de41eff797f1
adamhitzger <adam.hitzger@icloud.com>
2026-04-15 17:11:18 +0200

    Comgate

--- lib/comgate/client.ts
+++ lib/comgate/client.ts
@@ -37,7 +37,7 @@ export class ComgateRestClient {
     }
 
     public async statusOfPayment(transId: string): Promise<ComgateStatusResponse> {
-        const res = await this.axios.post(`payment/transId/${transId}.json`,{
+        const res = await this.axios.post(`payment/transId/${transId}.json`,null,{
             headers: { 'Authorization': this.createSignature() },
         });
 
